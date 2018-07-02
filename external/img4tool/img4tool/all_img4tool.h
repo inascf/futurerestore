d2a1aa9ced459d8bd758f6dfc663233370b89c30
xybp888 <8657156@qq.com>
2018-07-03 01:54:39 +0800

    add tsscheker and idevicerestore and img4tool
    add tsscheker and idevicerestore and img4tool

---
+++ external/img4tool/img4tool/all_img4tool.h
@@ -0,0 +1,19 @@
+//
+//  all.h
+//  img4tool
+//
+//  Created by tihmstar on 15.06.16.
+//  Copyright © 2016 tihmstar. All rights reserved.
+//
+
+#ifndef all_h
+#define all_h
+
+#define error(a ...) printf("[Error] %s: ",__func__),printf(a)
+#define warning(a ...) printf("[Warning] %s: ",__func__),printf(a)
+
+#define VERSION_COMMIT_COUNT "88"
+#define VERSION_COMMIT_SHA "e7820b4a6fe13dcf0ba2f9eeea735df2a9ffc134"
+
+
+#endif /* all_h */
